89b38aff1df2f8e28a6f4e8e7aa903ab1f6e13b5
Gaya <gaya@web-hamadi.fr>
2020-03-17 11:36:50 +0100

    Maj

--- controller/index.php
+++ controller/index.php
@@ -20,7 +20,17 @@
         </div>
       </div>
       <div class="head">
-
+        <div class="part">
+          <h1>Créez votre projet de A à Z avec STRTOWEB</h1>
+          <h2>Nous avons réaliser StrToWeb.com pour pouvoir moduler votre site internet, Autoriser les paiements en ligne, et automatiser les actions.Le site une version temporaire est ne represente pas la version finale.</h2>
+          <div class="btns">
+            <a class="btn-head">Estimer mon site</a>
+            <a class="btn-head2">Nos sites</a>
+          </div>
+        </div>
+        <div class="part">
+          <img src="https://themes.startbootstrap.com/sb-ui-kit-pro/assets/img/drawkit/color/drawkit-content-man-color.svg" alt="">
+        </div>
       </div>
       <svg class="svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 144.54 17.34" preserveAspectRatio="none" fill="#fff"><path d="M144.54,17.34H0V0H144.54ZM0,0S32.36,17.34,72.27,17.34,144.54,0,144.54,0"></path></svg>
     </div>
