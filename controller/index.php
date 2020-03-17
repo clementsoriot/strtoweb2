15b87bd359d7908b4b345a845e6d6a1900c7adb8
Gaya <gaya@web-hamadi.fr>
2020-03-17 12:40:40 +0100

    Maj

--- controller/index.php
+++ controller/index.php
@@ -6,38 +6,65 @@
     <link rel="stylesheet" href="<?= siteurl ?>/src/main.css/">
     <link rel="stylesheet" href="<?= siteurl ?>/src/mobile.css/">
     <script src="https://use.fontawesome.com/94cfaff88f.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   </head>
   <body>
     <div class="header">
       <div class="nav">
-        <div class="part">
-          <h1>STRTOWEB</h1>
-        </div>
-        <div class="part pc">
-          <p class="link">Accueil</p>
-          <p class="link">Sites partenaires</p>
-          <p class="link">F.A.Q</p>
-          <p class="link">Nous Contacter</p>
-          <a class="btn link">Espace Client</a>
-        </div>
-        <div class="part mobile">
-          <i class="fa fa-bars" aria-hidden="true"></i>
+        <div class="container">
+          <div class="part">
+            <h1>STRTOWEB</h1>
+          </div>
+          <div class="part pc">
+            <p class="link">Accueil</p>
+            <p class="link">Sites partenaires</p>
+            <p class="link">F.A.Q</p>
+            <p class="link">Nous Contacter</p>
+            <a class="btn link">Espace Client</a>
+          </div>
+          <div class="part mobile">
+            <i class="fa fa-bars hamburger-icon" aria-hidden="true"></i>
+          </div>
         </div>
       </div>
       <div class="head">
-        <div class="part">
-          <h1>Créez votre projet de A à Z avec STRTOWEB</h1>
-          <h2>Nous avons réaliser StrToWeb.com pour pouvoir moduler votre site internet, Autoriser les paiements en ligne, et automatiser les actions.Le site une version temporaire est ne represente pas la version finale.</h2>
-          <div class="btns">
-            <a class="btn-head">Estimer mon site</a>
-            <a class="btn-head2">Nos sites</a>
+        <div class="dropdown">
+          <div class="container">
+            <p class="link">Accueil</p>
+            <p class="link">Sites partenaires</p>
+            <p class="link">F.A.Q</p>
+            <p class="link">Nous Contacter</p>
+            <a class="btn link">Espace Client</a>
           </div>
         </div>
-        <div class="part pc">
-          <img src="https://themes.startbootstrap.com/sb-ui-kit-pro/assets/img/drawkit/color/drawkit-content-man-color.svg" alt="">
+        <div class="container">
+          <div class="part" style="display:flex;">
+            <h1>Créez votre projet de A à Z avec STRTOWEB</h1>
+            <h2>Nous avons réaliser StrToWeb.com pour pouvoir moduler votre site internet, Autoriser les paiements en ligne, et automatiser les actions.Le site une version temporaire est ne represente pas la version finale.</h2>
+            <div class="btns">
+              <a class="btn-head">Estimer mon site</a>
+              <a class="btn-head2">Nos sites</a>
+            </div>
+          </div>
+          <div class="part pc">
+            <img src="https://themes.startbootstrap.com/sb-ui-kit-pro/assets/img/drawkit/color/drawkit-content-man-color.svg" alt="">
+          </div>
         </div>
       </div>
       <svg class="svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 144.54 17.34" preserveAspectRatio="none" fill="#fff"><path d="M144.54,17.34H0V0H144.54ZM0,0S32.36,17.34,72.27,17.34,144.54,0,144.54,0"></path></svg>
     </div>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
   </body>
 </html>
+<script type="text/javascript">
+
+$(".dropdown").slideUp();
+$( window ).resize(function() {
+  $(".dropdown").slideUp();
+});
+
+$(".hamburger-icon").click(function(){
+  $(".dropdown").slideToggle();
+});
+
+</script>
