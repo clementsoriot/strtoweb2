2841fcf89389d470850f3345cf610aaa2114ca5a
Gaya <gaya@web-hamadi.fr>
2020-03-17 10:54:04 +0100

    Maj

---
+++ controller/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>STRTOWEB : lpb de tout les rebeux</title>
+    <link rel="stylesheet" href="main.css">
+  </head>
+  <body>
+    <div class="header">
+      <div class="nav">
+        <div class="part">
+          <h1>STRTOWEB</h1>
+        </div>
+        <div class="part">
+          <p>Accueil</p>
+          <p>Sites partenaires</p>
+          <p>F.A.Q</p>
+          <p>Nous Contacter</p>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
